435917f640864730fb5cd046d3e2231a2e63b7bc
Leo Furze-Waddock <leo@furze-waddock.com>
2020-02-16 12:09:36 +0000

    fix: missing config object ref

--- packages/ui/src/modules/ajax-base-api-app.js
+++ packages/ui/src/modules/ajax-base-api-app.js
@@ -3,5 +3,5 @@ import axios from "axios";
 import config from "../app-config";
 
 export default axios.create({
-  baseURL: `${config.baseUrl[process.env.NODE_ENV || "development"]}`,
+  baseURL: `${config.api.baseUrl[process.env.NODE_ENV || "development"]}`,
 });
